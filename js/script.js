c558bafb2de9c7278ad3fcce8e607a9c0282f2d0
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2023-03-23 00:02:12 +0530

    all cards created

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 
 
-function togglePopup(){
+function togglePopup() {
     document.querySelector('.full-screen-popup').classList.toggle('hidden');
 }
 
@@ -12,28 +12,28 @@ let isDragging = false;
 let startX, scrollLeft, container;
 
 document.addEventListener("mousedown", (e) => {
-  container = e.target.closest("#project-cards");
-  if (container) {
-    isDragging = true;
-    startX = e.pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-  }
+    container = e.target.closest("#experience-cards,#project-cards, #achievement-cards,#certification-cards");
+    if (container) {
+        isDragging = true;
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+    }
 });
 
 document.addEventListener("mouseup", () => {
-  isDragging = false;
+    isDragging = false;
 });
 
 document.addEventListener("mouseleave", () => {
-  isDragging = false;
+    isDragging = false;
 });
 
 document.addEventListener("mousemove", (e) => {
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX);
-  container.scrollLeft = scrollLeft - walk;
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX);
+    container.scrollLeft = scrollLeft - walk;
 });
 
 
